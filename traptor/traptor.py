5c2ea8c2124b0048bfa8b9e3183809d5d95ed082
Jason Haas <jasonrhaas@gmail.com>
2015-09-02 18:01:12 -0400

    improve error handling

--- traptor/traptor.py
+++ traptor/traptor.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from birdy.twitter import StreamClient
+from birdy import twitter
 import json
-import redis
+from redis import StrictRedis, ConnectionError
 import logging
+import sys
 
 from kafka import SimpleProducer, KafkaClient
 from settings import KAFKA_HOSTS, KAFKA_TOPIC, APIKEYS, TRAPTOR_ID, TRAPTOR_TYPE, REDIS_HOST
@@ -19,16 +21,21 @@ logger.setLevel(logging.INFO)
 # logging.basicConfig(level=logging.CRITICAL)
 # logging.getLogger(__main__)
 
-r = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0)
 
+def sscanit(traptor_type, traptor_id, redis_host=REDIS_HOST):
+    r = StrictRedis(host=redis_host, port=6379, db=0)
 
-def sscanit(traptor_type, traptor_id):
     twids = []
     redis_key = 'traptor-{0}:{1}'.format(traptor_type, traptor_id)
-    for idx, key in enumerate(r.sscan_iter(redis_key)):
-        if idx < 5000:
-            twids.append(key)
-            logger.debug('{0}: {1}'.format(idx, key))
+    try:
+        for idx, key in enumerate(r.sscan_iter(redis_key)):
+            if idx < 5000:
+                twids.append(key)
+                logger.debug('{0}: {1}'.format(idx, key))
+    except ConnectionError as e:
+        logger.critical(e)
+        # 3 is still an error code, but supervisor will not try to restart
+        sys.exit(3)
     return twids
 
 
@@ -57,13 +64,19 @@ def kafka_producer():
 
 def run(traptor_type=TRAPTOR_TYPE, traptor_id=TRAPTOR_ID):
     twids_str = ','.join(sscanit(traptor_type, traptor_id))
+    logger.debug('Twitter ids: {0}'.format(twids_str))
     client = StreamClient(APIKEYS['CONSUMER_KEY'],
                           APIKEYS['CONSUMER_SECRET'],
                           APIKEYS['ACCESS_TOKEN'],
                           APIKEYS['ACCESS_TOKEN_SECRET']
                           )
     if traptor_type == 'follow':
-        resource = client.stream.statuses.filter.post(follow=twids_str)
+        try:
+            resource = client.stream.statuses.filter.post(follow=twids_str)
+        except twitter.TwitterApiError as e:
+            logger.critical(e)
+            # 3 is still an error code, but supervisor will not try to restart
+            sys.exit(3)
     elif traptor_type == 'track':
         sys.exit('track not implemented yet')
     else:
