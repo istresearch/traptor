85c5e6502c9e6aa37b9f57b08c0360a815ba046c
Jason Haas <jasonrhaas@gmail.com>
2015-09-11 11:45:15 -0400

    make fabfile for easy pip package deploys

---
+++ fabfile.py
@@ -0,0 +1,46 @@
+from fabric.api import local
+
+""" 
+    Overview
+    ========
+
+    This fabric file automates the process of pip packaging and
+    deploying your new pip package to our private pip repository.
+
+    Requirements
+    ------------
+
+    - Must have fabric installed via `pip install fabric`
+    - Must have your setup.py working and up to date.  Make sure
+      it works by running `python setup.py test` or do a test install
+      via `python setup.py install` inside a virtualenv.
+
+    Deploying
+    ---------
+
+    Run `fab publish` for a one step pip package deploy!
+"""
+
+pip_repo = 'memexdev.istresearch.com:/var/www/ild-repo/pip'
+dist_dir = 'dist'
+
+
+def prep():
+    local("pip install pip2pi")
+
+
+def package():
+    local("python setup.py sdist")
+
+
+def deploy():
+    name = local("python setup.py --name", capture=True)
+    ver = local("python setup.py --version", capture=True)
+    sdist_name = '{}-{}.tar.gz'.format(name, ver)
+    local("pip2pi {} {}/{}".format(pip_repo, dist_dir, sdist_name))
+
+
+def publish():
+    prep()
+    package()
+    deploy()
