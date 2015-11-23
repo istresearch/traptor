3a65cbc2119ecbcb9bba94783af6db0c2c1a25e8
Jason Haas <jasonrhaas@gmail.com>
2015-11-23 14:49:18 -0500

    Unit testing improvements
    Use unittest test cases.  Add more tests to parsing logic.

--- tests/test_rule_extract.py
+++ tests/test_rule_extract.py
@@ -1,27 +1,63 @@
-from nose.tools import assert_equal
-from scripts.rule_extract import CooperRules, send_to_redis
-
-follow_rule_0 = {
-    'tag': 'marketing',
-    'value': 'from:345345234 OR someguy'
-}
-
-track_rule_0 = {
-    'tag': 'short link',
-    'value': 'url_contains: dump.to'
-}
-
-follow_rules = [follow_rule_0]
-track_rules = [track_rule_0]
+# from nose.tools import assert_equal
+from unittest import TestCase
+from mock import MagicMock
+from scripts.rule_extract import CooperRules, RulesToRedis
 
 
-def test_fix_follow():
-    """ Testing that the "follow" rules get normalized to Twitter format. """
-    fixed_rules = CooperRules._fix_follow(follow_rules)
-    assert_equal(fixed_rules, [{'tag': 'marketing', 'value': '345345234'}])
+class TestRuleExtract(TestCase):
 
+    def setUp(self):
+        follow_rule_0 = {
+            'tag': 'marketing',
+            'value': 'from:345345234 OR someguy'
+        }
 
-def test_fix_track():
-    """ Testing that the "track" rules get normalized to Twitter format. """
-    fixed_rules = CooperRules._fix_track(track_rules)
-    assert_equal(fixed_rules, [{'tag': 'short link', 'value': 'dump to'}])
+        follow_rule_1 = {
+            'tag': 'sales',
+            'value': 'someguy'
+        }
+
+        follow_rule_2 = {
+            'tag': 'random',
+            'value': 'from:34534509889 OR someguy'
+        }
+
+        track_rule_0 = {
+            'tag': 'short link',
+            'value': 'url_contains: dump.to'
+        }
+
+        track_rule_1 = {
+            'tag': 'ref_keywords',
+            'value': 'something random'
+        }
+
+        self.follow_rules = [follow_rule_0, follow_rule_1, follow_rule_2]
+        self.track_rules = [track_rule_0, track_rule_1]
+
+        self.fixed_follow_rules = [
+                         {'tag': 'marketing', 'value': '345345234'},
+                         {'tag': 'random', 'value': '34534509889'}
+                         ]
+        self.fixed_track_rules = [
+                         {'tag': 'short link', 'value': 'dump to'},
+                         {'tag': 'ref_keywords', 'value': 'something random'}
+                         ]
+
+    def test_fix_follow(self):
+        """ Testing that the "follow" rules get normalized to Twitter format.
+        """
+        fixed_rules = CooperRules._fix_follow(self.follow_rules)
+        self.assertEqual(fixed_rules, self.fixed_follow_rules)
+
+    def test_fix_track(self):
+        """ Testing that the "track" rules get normalized to Twitter format.
+        """
+        fixed_rules = CooperRules._fix_track(self.track_rules)
+        self.assertEqual(fixed_rules, self.fixed_track_rules)
+
+    def test_send_to_redis(self):
+        """ Testing the logic that sends the rules to Redis. """
+        # rc1 = RulesToRedis('track', self.fixed_track_rules)
+        raise NotImplementedError
+
