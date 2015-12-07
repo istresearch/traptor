f94d82a1336fa750ca85799ec7426f34a589e9f3
Jason Haas <jasonrhaas@gmail.com>
2015-12-07 15:57:05 -0500

    Complete re-write of traptor module
    Traptor is now a class, and every logical process  is broken out into
    its own method.  To run a traptor, use the `traptor.run()` method after
    instantiating a traptor instance.

--- traptor/traptor.py
+++ traptor/traptor.py
@@ -12,184 +12,304 @@ from kafka.common import (NotLeaderForPartitionError, KafkaUnavailableError)
 from birdy.twitter import StreamClient, TwitterApiError
 import click
 
-from scutils.log_factory import LogObject
+from scutils.log_factory import LogFactory
 
 from settings import (KAFKA_HOSTS, KAFKA_TOPIC, APIKEYS, TRAPTOR_ID,
-                      TRAPTOR_TYPE, REDIS_HOST)
-
-logger = LogObject(name='traptor', level='INFO')
+                      TRAPTOR_TYPE, REDIS_HOST, REDIS_PORT, REDIS_DB,
+                      LOG_LEVEL)
 
 
 # Override the default JSONobject
-class MyClient(StreamClient):
+class MyBirdyClient(StreamClient):
     @staticmethod
     def get_json_object_hook(data):
         return data
 
 
-def get_redis_twitter_rules(traptor_type=TRAPTOR_TYPE, traptor_id=TRAPTOR_ID,
-                            redis_host=REDIS_HOST):
-    """ Return a list of twitter ids from the service server.  This function
-        expects that the redis keys are set up like follows:
+class Traptor(object):
 
-        traptor-<traptor_type>:<crawler_num>
+    def __init__(self,
+                 apikeys=None,
+                 traptor_type='track',
+                 traptor_id=0,
+                 kafka_hosts='localhost:9092',
+                 kafka_topic='traptor',
+                 redis_host='localhost',
+                 redis_port=6379,
+                 redis_db=0,
+                 kafka_enabled=True,
+                 log_level='INFO'
+                 ):
+        """
+        Traptor base class.
 
-        For example,
+        :param int traptor_id: numerical ID of traptor instance
+        :param str traptor_type: follow, track, or geo
+        """
+        self.apikeys = apikeys
+        self.traptor_type = traptor_type
+        self.traptor_id = traptor_id
+        self.kafka_hosts = kafka_hosts
+        self.kafka_topic = kafka_topic
+        self.redis_host = redis_host
+        self.redis_port = redis_port
+        self.redis_db = redis_db
+        self.kafka_enabled = kafka_enabled
+        self.log_level = log_level
 
-        traptor-follow:0
-        traptor-follow:1
+    def setup(self, level='INFO', log_file=None, json=None):
+        """
+        Load everything up. Note that any arg here will override both
+        default and custom settings
 
-        traptor-track:0
-        traptor-track:1
+        :param str level: the log level
+        :param bool log_file: boolean t/f whether to log to a file, else stdout
+        :param bool json: boolean t/f whether to write the logs in json
+        """
 
-        For 'follow' twitter streaming, each traptor may only
-        follow 5000 twitter ids, as per the Twitter API.
+        # Set up logging
+        # my_level = level if level else self.settings['LOG_LEVEL']
+        self.logger = LogFactory.get_instance(name='traptor', level=level)
 
-        For 'track' twitter stream, each traptor may only
-        track 400 keywords, as per the Twitter API.
-    """
-    # Set up API limitation checks
-    if traptor_type == 'follow':
-        rule_max = 5000
-    elif traptor_type == 'track':
-        rule_max = 400
-    else:
-        logger.error('traptor_type of {0} is not supported'.format(
-                     traptor_type))
-        return list()
+        # Set up all connections
+        self._setup_redis(self)
+        self._setup_kafka(self)
+        self._setup_birdy(self)
 
-    # This line is lazy, nothing touches Redis until a command is issued
-    r = StrictRedis(host=redis_host, port=6379, db=0)
+    def _setup_birdy(self):
+        """ Set up a birdy twitter stream.
+            If there is a TwitterApiError it will exit with status code 3.
+            This was done to prevent services like supervisor from automatically
+            restart the process causing the twitter API to get locked out.
+        """
 
-    twids = []
-    # for rule in xrange(rule_max):
-    redis_key = 'traptor-{0}:{1}'.format(traptor_type, traptor_id)
-    match = ':'.join([redis_key, '*'])
-    try:
-        for idx, hashname in enumerate(r.scan_iter(match=match)):
-            if idx < rule_max:
-                twids.append(r.hgetall(hashname))
-                logger.debug('{0}: {1}'.format(idx, hashname))
-    except ConnectionError as e:
-        logger.critical(e)
-        sys.exit(3)  # Special error code to track known failures
+        # Set up a birdy twitter streaming client
+        self.birdy_conn = MyBirdyClient(
+                                        self.apikeys['CONSUMER_KEY'],
+                                        self.apikeys['CONSUMER_SECRET'],
+                                        self.apikeys['ACCESS_TOKEN'],
+                                        self.apikeys['ACCESS_TOKEN_SECRET']
+                                        )
 
-    return twids
+    def _setup_kafka(self):
+        """ Set up a Kafka connection. """
+        if self.kafka_enabled:
+            self.kafka_conn = KafkaClient(hosts=self.kafka_hosts)
+        else:
+            self.kafka_conn = None
 
+    def _setup_redis(self):
+        """Set up a Redis connection
+            This line is lazy, nothing touches Redis until a command is issued.
+        """
+        self.redis_conn = StrictRedis(host=self.redis_host,
+                                      port=self.redis_port,
+                                      db=self.redis_db)
 
-def create_kafka_producer(kafka_hosts=KAFKA_HOSTS, kafka_topic=KAFKA_TOPIC):
-    """ Create a kafka producer.
-        If it cannot find one it will exit with error code 3.
-    """
-    try:
-        client = KafkaClient(hosts=kafka_hosts)
-        producer = SimpleProducer(client)
-    except KafkaUnavailableError as e:
-        logger.critical(e)
-        sys.exit(3)
-    try:
-        client.ensure_topic_exists(kafka_topic)
-    except:
-        raise
+    def _create_kafka_producer(self, kafka_topic):
+        """ Create a kafka producer.
+            If it cannot find one it will exit with error code 3.
 
-    return producer
-
-
-def create_birdy_stream(rules,
-                        apikeys=APIKEYS,
-                        traptor_type=TRAPTOR_TYPE,
-                        traptor_id=TRAPTOR_ID,
-                        ):
-    """ Set up a birdy twitter stream.
-        If there is a TwitterApiError it will exit with status code 3.
-        This was done to prevent services like supervisor from automatically
-        restart the process causing the twitter API to get locked out.
-    """
-
-    # Set up a birdy twitter streaming client
-    client = MyClient(
-                          APIKEYS['CONSUMER_KEY'],
-                          APIKEYS['CONSUMER_SECRET'],
-                          APIKEYS['ACCESS_TOKEN'],
-                          APIKEYS['ACCESS_TOKEN_SECRET']
-                      )
-    if traptor_type == 'follow':
-        # Try to set up a twitter stream using twitter id list
+            Creates self.kafka_producer
+        """
         try:
-            resource = client.stream.statuses.filter.post(follow=rules)
-            return resource
-        except TwitterApiError as e:
-            logger.critical(e)
+            self.kafka_producer = SimpleProducer(self.kafka_conn)
+        except KafkaUnavailableError as e:
+            self.logger.critical(e)
             sys.exit(3)
-    elif traptor_type == 'track':
-        # Try to set up a twitter stream using twitter term list
         try:
-            resource = client.stream.statuses.filter.post(track=rules)
-            return resource
-        except TwitterApiError as e:
-            logger.critical(e)
+            self.kafka_conn.ensure_topic_exists(self.kafka_topic)
+        except:
+            raise
+
+    def _create_birdy_stream(self):
+        """ Create a birdy twitter stream.
+            If there is a TwitterApiError it will exit with status code 3.
+            This was done to prevent services like supervisor from automatically
+            restart the process causing the twitter API to get locked out.
+
+            Creates self.birdy_stream
+        """
+
+        if self.traptor_type == 'follow':
+            # Try to set up a twitter stream using twitter id list
+            try:
+                self.birdy_stream = self.birdy_conn.stream.statuses.filter.post(follow=self.twitter_rules)
+            except TwitterApiError as e:
+                self.logger.critical(e)
+                sys.exit(3)
+        elif self.traptor_type == 'track':
+            # Try to set up a twitter stream using twitter term list
+            try:
+                self.birdy_stream = self.birdy_conn.stream.statuses.filter.post(track=self.twitter_rules)
+            except TwitterApiError as e:
+                self.logger.critical(e)
+                sys.exit(3)
+        else:
+            self.logger.critical('That traptor type has not been implemented yet')
             sys.exit(3)
-    else:
-        logger.critical('That traptor type has not been implemented yet')
-        sys.exit(3)
 
-def tweet_time_to_iso(tweet_time):
-    return parser.parse(tweet_time).isoformat()
+    def write_to_kafka(self):
+        pass
 
-def clean_tweet_data(tweet_dict):
-    """ Do any pre-processing to raw tweet data before passing on
-        to Kafka
-    """
+    def write_to_stdout(self):
+        pass
 
-    if tweet_dict.get('created_at'):
-        tweet_dict['created_at'] = tweet_time_to_iso(tweet_dict['created_at'])
-    return tweet_dict
+    @staticmethod
+    def _make_twitter_rules(rules):
+        rules_str = ','.join([rule['value'] for rule in rules])
+        return rules_str
 
-
-@click.command()
-@click.option('--test', is_flag=True)
-def run(test):
-    # Grab a list of {tag:, value:} rules
-    rules = get_redis_twitter_rules()
-    logger.debug(rules)
-
-    # Concatenate all of the rule['value'] fields
-    rules_str = ','.join([rule['value'] for rule in rules])
-    logger.debug(rules_str)
-
-    if not test:
-        # Set up Kafka producer
-        producer = create_kafka_producer()
-
-        # Set up a birdy streaming client
-        time.sleep(60)
-
-    birdyclient = create_birdy_stream(rules_str)
-
-    # Iterate through the twitter results
-    for _data in birdyclient.stream():
-        logger.debug('Raw Data: {0}'.format(json.dumps(_data)))
-
-        # Do tweet data pre-processing
-        data = clean_tweet_data(_data)
-
-        # Find which rule the tweet matched.  This code only expects there to
-        # be one match.  If there is more than one, it will use the last one
-        # it finds since the first match will be overwritten.
-        for rule in rules:
+    def _find_rule_matches(self, data):
+        """ Find which rule the tweet matched.  This code only expects there to
+            be one match.  If there is more than one, it will use the last one
+            it finds since the first match will be overwritten.
+        """
+        for rule in self.redis_rules:
             search_str = rule['value'].split()[0]
             if re.search(search_str, json.dumps(data)):
                 data['rule_tag'] = rule['tag']
                 data['rule_value'] = rule['value']
 
-        logger.info('utf-8 Text: {0}'.format(data.get('text').encode('utf-8')))
-        logger.info('Rule matched - tag:{}, value:{}'.format(
+        self.logger.info('utf-8 Text: {0}'.format(
+                         data.get('text').encode('utf-8')))
+        self.logger.info('Rule matched - tag:{}, value:{}'.format(
                     data.get('rule_tag'), data.get('rule_value')))
-        logger.debug('Cleaned Data: {0}'.format(json.dumps(data)))
+        self.logger.debug('Cleaned Data: {0}'.format(json.dumps(data)))
 
-        if not test:
-            # Send to Kafka
-            producer.send_messages(KAFKA_TOPIC, json.dumps(data))
+        return data
+
+    def _get_redis_rules(self):
+        """ Return a list of twitter ids from the service server.  This function
+            expects that the redis keys are set up like follows:
+
+            traptor-<traptor_type>:<crawler_num>
+
+            For example,
+
+            traptor-follow:0
+            traptor-follow:1
+
+            traptor-track:0
+            traptor-track:1
+
+            For 'follow' twitter streaming, each traptor may only
+            follow 5000 twitter ids, as per the Twitter API.
+
+            For 'track' twitter stream, each traptor may only
+            track 400 keywords, as per the Twitter API.
+        """
+        # Set up API limitation checks
+        if self.traptor_type == 'follow':
+            rule_max = 5000
+        elif self.traptor_type == 'track':
+            rule_max = 400
+        else:
+            self.logger.error('traptor_type of {0} is not supported'.format(
+                         self.traptor_type))
+            return list()
+
+        twids = []
+        # for rule in xrange(rule_max):
+        redis_key = 'traptor-{0}:{1}'.format(self.traptor_type,
+                                             self.traptor_id)
+        match = ':'.join([redis_key, '*'])
+        try:
+            for idx, hashname in enumerate(self.redis_conn.scan_iter(match=match)):
+                if idx < rule_max:
+                    twids.append(self.redis_conn.hgetall(hashname))
+                    self.logger.debug('{0}: {1}'.format(idx, hashname))
+        except ConnectionError as e:
+            self.logger.critical(e)
+            sys.exit(3)  # Special error code to track known failures
+
+        return twids
+
+    @staticmethod
+    def _tweet_time_to_iso(tweet_time):
+        return parser.parse(tweet_time).isoformat()
+
+    def _clean_tweet_data(self, tweet_dict):
+        """ Do any pre-processing to raw tweet data before passing on
+            to Kafka
+        """
+
+        if tweet_dict.get('created_at'):
+            tweet_dict['created_at'] = self._tweet_time_to_iso(
+                                                    tweet_dict['created_at'])
+        return tweet_dict
+
+    def _main_loop(self):
+        # Iterate through the twitter results
+        for _data in self.birdy_stream.stream():
+            self.logger.debug('Raw Data: {0}'.format(json.dumps(_data)))
+
+            # Do tweet data pre-processing
+            data = self._clean_tweet_data(_data)
+
+            # Do any data enrichment on the base tweet data
+            enriched_data = self._find_rule_matches(data)
+            self.logger.info('Text: {}'.format(
+                             json.dumps(enriched_data['text'])))
+
+            if self.kafka_enabled:
+                self.kafka_producer.send_messages(self.kafka_topic,
+                                                  json.dumps(data))
+
+    def run(self):
+        # Set up logging
+        self.logger = LogFactory.get_instance(name='traptor',
+                                              level=self.log_level)
+
+        # Set up required connections
+        self._setup_redis()
+        self._setup_birdy()
+        if self.kafka_enabled:
+            self._setup_kafka()
+
+        # Grab a list of {tag:, value:} rules
+        self.redis_rules = self._get_redis_rules()
+
+        # Concatenate all of the rule['value'] fields
+        self.twitter_rules = self._make_twitter_rules(self.redis_rules)
+
+        # Create bridy and kafka connections
+        self._create_birdy_stream()
+        if self.kafka_enabled:
+            self._create_kafka_producer(self.kafka_topic)
+
+        # Start collecting data
+        self._main_loop()
+
+
+@click.command()
+@click.option('--test', is_flag=True)
+@click.option('--info', is_flag=True)
+@click.option('--debug', is_flag=True)
+def main(test, info, debug):
+    kafka_enabled = False if test else True
+    if debug:
+        log_level = 'DEBUG'
+    elif info:
+        log_level = 'INFO'
+    else:
+        log_level = 'CRITICAL'
+
+    traptor_instance = Traptor(apikeys=APIKEYS,
+                               traptor_type=TRAPTOR_TYPE,
+                               traptor_id=TRAPTOR_ID,
+                               kafka_hosts=KAFKA_HOSTS,
+                               redis_host=REDIS_HOST,
+                               redis_port=REDIS_PORT,
+                               redis_db=REDIS_DB,
+                               kafka_enabled=kafka_enabled,
+                               log_level=log_level
+                               )
+
+    # Setup Logging, Redis, and Kafka connections
+    # traptor_instance.setup(level=LOG_LEVEL)
+    # Run the traptor instance and start collecting data
+    traptor_instance.run()
 
 if __name__ == '__main__':
-    run()
+    sys.exit(main())
