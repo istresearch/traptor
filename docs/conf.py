07c8b95f7b9d0f672721b15a1c6baa4b80a7870e
Jason Haas <jasonrhaas@gmail.com>
2015-12-10 11:22:17 -0500

    Add shlex back into conf.py imports

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import sphinx_rtd_theme
+import shlex
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
