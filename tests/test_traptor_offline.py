aa872ee9347d6035be132b9fabc16983e0e6fa03
Robert Dempsey <robertonrails@gmail.com>
2016-05-20 17:31:49 -0400

    TravisCI needs localhost
    I need to make this locally configurable…

--- tests/test_traptor_offline.py
+++ tests/test_traptor_offline.py
@@ -13,7 +13,7 @@ from traptor.traptor import Traptor, MyBirdyClient
 from scripts.rule_extract import RulesToRedis
 from scutils.log_factory import LogObject
 
-HOST_FOR_TESTING = 'scdev'
+HOST_FOR_TESTING = 'localhost'
 
 
 @pytest.fixture()
