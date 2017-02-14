5cb592bcefeefab9bcb2fbe8725a08a03bf245a2
Robert Dempsey <robertonrails@gmail.com>
2017-02-14 11:31:21 -0500

    Shuffle rules before performing a rule match
    Before track and follow Traptors perform a rule match, they will
    shuffle the redis rules (a list). Tests in a Jupyter Notebook show that
    a shuffle of up to 400 rules takes about 256 microseconds (performed
    with 1000 loops).

--- tests/test_traptor_offline.py
+++ tests/test_traptor_offline.py
@@ -237,6 +237,7 @@ class TestTraptor(object):
     def test_main_loop(self, traptor, tweets):
         """Ensure we can loop through the streaming Twitter data."""
         traptor._setup()
+
         traptor.redis_rules = [rule for rule in traptor._get_redis_rules()]
         traptor.twitter_rules = traptor._make_twitter_rules(traptor.redis_rules)
 
@@ -286,21 +287,6 @@ class TestTraptor(object):
             assert enriched_data['traptor']['description'] == 'Tweets from some continent'
             assert enriched_data['traptor']['appid'] == 'test-appid'
 
-        """ Now test the with the new extended_tweet format """
-
-        # # The birdy_stream will just look like whatever tweet has been loaded
-        # traptor.birdy_stream = MagicMock(return_value=extended_tweets)
-        # traptor.birdy_stream.stream = traptor.birdy_stream
-
-        # # Test that we can set the tweet to the .stream() method
-        # tweet = traptor.birdy_stream.stream()
-
-        # # Do the rule matching against the redis rules
-        # enriched_data = traptor._enrich_tweet(tweet)
-
-        # if traptor.traptor_type == 'follow':
-        #     assert enriched_data['traptor'] == x
-
 
     @pytest.mark.extended
     # @pytest.mark.parametrize('traptor', ['follow'])
@@ -322,7 +308,7 @@ class TestTraptor(object):
             # Do the rule matching against the redis rules
             enriched_data = traptor._enrich_tweet(tweet)
 
-            assert enriched_data['traptor']['value'] == '735369652956766200'
+            assert enriched_data['traptor']['rule_value'] == '735369652956766200'
 
         if traptor.traptor_type == 'track':
             with open('tests/data/extended_tweets/track_rules.json') as f:
@@ -332,7 +318,7 @@ class TestTraptor(object):
             # Do the rule matching against the redis rules
             enriched_data = traptor._enrich_tweet(tweet)
 
-            assert enriched_data['traptor']['value'] == 'tweet'
+            assert enriched_data['traptor']['rule_value'] == 'tweet'
 
     def test_ensure_traptor_is_in_tweet_on_no_match(self, traptor, no_match_tweet):
         """Ensure that the traptor section is added to a tweet when no rule matches."""
